51211e70bc55b09908ba5b1870753458f2a2a912
NightW0lfff <philong170101@gmail.com>
2023-05-19 23:47:09 +1000

    Updated Add Grocery List

--- app/src/main/java/com/example/housechefv03/AddGroceryItem.java
+++ app/src/main/java/com/example/housechefv03/AddGroceryItem.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.DialogFragment;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,8 +21,12 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -30,8 +36,9 @@ public class AddGroceryItem extends AppCompatActivity {
     ArrayAdapter<String> itemsAdapter;
     ListView listView;
     Button button;
-    TextView text;
+    TextView text, listTitleView;
     DatabaseReference databaseReference;
+    String listTitle;
 
 
 
@@ -40,16 +47,21 @@ public class AddGroceryItem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grocery_item);
 
+        listTitle = getIntent().getStringExtra("ListTitle");
+
         listView = findViewById(R.id.groceryListView);
         button = findViewById(R.id.btn_addItem);
         text = findViewById(R.id.backText);
+        listTitleView = findViewById(R.id.listTitle);
+
+        listTitleView.setText(listTitle);
 
         databaseReference = FirebaseDatabase.getInstance().getReference("GroceryItems");
 
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showUnsavedChangesDialog();
+                finish();
             }
         });
 
@@ -63,8 +75,10 @@ public class AddGroceryItem extends AppCompatActivity {
         items = new ArrayList<>();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(itemsAdapter);
-        setUplistViewListener();
+//        setUplistViewListener();
     }
+
+
 
     private void setUplistViewListener() {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -74,11 +88,12 @@ public class AddGroceryItem extends AppCompatActivity {
                 Toast.makeText(context,"Item Removed", Toast.LENGTH_LONG).show();
 
                 // Remove the item from list view
-//                removeItemFromDatabase(i);
+
                 items.remove(i);
                 itemsAdapter.notifyDataSetChanged();
-
                 // Remove the item from the database
+                removeItemFromDatabase(i);
+
                 return true;
             }
         });
@@ -101,13 +116,13 @@ public class AddGroceryItem extends AppCompatActivity {
 
     private void saveItemToDatabase(String itemText) {
         // Generate a unique key for the item
-        String itemId = databaseReference.push().getKey();
+        String itemId = databaseReference.child(listTitle).push().getKey();
 
         // Create a GroceryItem object with the item name
         GroceryItem groceryItem = new GroceryItem(itemText);
 
         // Save the item to the database using the generated key
-        databaseReference.child(itemId).setValue(groceryItem)
+        databaseReference.child(listTitle).child(itemId).setValue(groceryItem)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
@@ -121,50 +136,39 @@ public class AddGroceryItem extends AppCompatActivity {
     }
 
 
-/*
+
     private void removeItemFromDatabase(int position) {
-        // Get the item key at the specified position
-        String itemKey = databaseReference.getKey(position);
+        // Get the item text at the specified position
+        String itemText = items.get(position);
 
-        // Remove the item from the database using the key
-        databaseReference.child(itemKey).removeValue()
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "Item removed from database", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Failed to remove item from database", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        // Find the item key in the database based on the item text
+        Query query = databaseReference.orderByChild("itemName").equalTo(itemText);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String itemKey = snapshot.getKey();
+
+                    // Remove the item from the database using the key
+                    databaseReference.child(itemKey).removeValue()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Item removed from database", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Failed to remove item from database", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Error querying database", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-*/
-
-
-    private void showUnsavedChangesDialog() {
-        if (!items.isEmpty()) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("You have unsaved changes. Are you sure you want to navigate back?")
-                    .setPositiveButton("Discard", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            finish();
-                        }
-                    })
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            // Do nothing, stay on the current screen
-                        }
-                    });
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        } else {
-            finish();
-        }
-    }
-
 
 }
